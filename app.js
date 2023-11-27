bd063507fee0c0c9c0acd6df4d21a2400a38b683
Peter MoDavis <pmodavis.webdev@gmail.com>
2023-11-27 14:21:24 -0500

    added initial files created on jsfiddle

---
+++ app.js
@@ -0,0 +1,34 @@
+const inputButton = document.querySelector('button');
+const paragraph = document.createElement('p');
+
+inputButton.addEventListener('click', (e) => {
+  const totalRevenueValue = document.querySelector('#total-revenue');
+  const venuePercentageValue = document.querySelector('#venue-percentage');
+  const container = document.querySelector('.container');
+
+  e.preventDefault();
+
+  paragraph.innerHTML = calculateBandSplit(
+    totalRevenueValue.value,
+    venuePercentageValue.value
+  );
+
+  container.append(paragraph);
+});
+
+function calculateBandSplit(total, venuePercentage) {
+  if (venuePercentage > 100 || venuePercentage < 0) {
+    alert('Please enter a value that is less then 100 and greater than 0');
+    return '';
+  } else {
+    const bandPercentage = (100 - venuePercentage) / 100;
+    const newVenuePercentage = venuePercentage / 100;
+
+    const bandRevenue = (total * bandPercentage).toFixed(2);
+    const venueRevenue = (total * newVenuePercentage).toFixed(2);
+
+    console.log(bandRevenue, venueRevenue);
+
+    return `Band revenue: <b>$${bandRevenue}</b> Venue revenue: <b>$${venueRevenue}</b>`;
+  }
+}
